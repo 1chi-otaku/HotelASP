70c13617c10217cbc11f71e0388b61eb1bd033bb
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-09 17:03:07 +0100

    Add project files.

---
+++ Hotel/Models/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace Hotel.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int Id_User { get; set; }
+        public Users? User { get; set; }
+
+        public string? Message { get; set; }
+
+        public DateTime? MessageDate {  get; set; }
+    }
+}
